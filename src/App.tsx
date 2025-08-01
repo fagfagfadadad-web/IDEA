564b801ca3d5a3c50efd04761077146bc47652fd
fagfagfadadad-web <fagfagfadadad@gmail.com>
2025-08-01 15:17:17 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,57 @@
-import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import { Home } from './Home';
-import { Gigs } from './Gigs';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { PageNotFound } from 'pages/PageNotFound/PageNotFound';
+import { routes } from 'routes';
+import { BatchTransactionsContextProvider } from 'wrappers';
+import { AuthProvider } from './context/AuthContext';
+import { Layout } from './components';
+import { MobileBottomNav } from './components/layout/MobileBottomNav';
 
-function App() {
+const AppContent = () => {
+  const location = useLocation();
+  
+  useEffect(() => {
+    console.log('🔄 App: Route changed to:', location.pathname);
+    console.log('🔄 App: Available routes:', routes.map(r => r.path));
+  }, [location]);
+
+  return (
+    <>
+      <Layout>
+        <Routes>
+          {routes.map((route) => (
+            <Route
+              key={`route-key-${route.path}`}
+              path={route.path}
+              element={<route.component />}
+            >
+              {route.children?.map((child) => (
+                <Route
+                  key={`route-key-${route.path}-${child.path}`}
+                  path={child.path}
+                  element={<child.component />}
+                />
+              ))}
+            </Route>
+          ))}
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </Layout>
+      <MobileBottomNav />
+    </>
+  );
+};
+
+export const App = () => {
+
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/gigs" element={<Gigs />} />
-        <Route path="/test" element={<div style={{ padding: 20, background: 'white' }}>Test Page Works!</div>} />
-        <Route path="*" element={<div style={{ padding: 20, background: 'white' }}>Page Not Found</div>} />
-      </Routes>
+      <AuthProvider>
+        <BatchTransactionsContextProvider>
+          <AppContent />
+        </BatchTransactionsContextProvider>
+      </AuthProvider>
     </Router>
   );
-}
-
-export default App;
+};
