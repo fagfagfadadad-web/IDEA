63a02ba64241de34fb9bc18284ad265522c30747
fagfagfadadad-web <fagfagfadadad@gmail.com>
2025-08-04 13:14:19 +0200

    Fix missing useTransactionOutcome export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,3 +2,4 @@ export * from './sdkCore';
 export * from './sdkDapp';
 export * from './sdkDappUI';
 export * from './sdkDappUtils';
+export { useTransactionOutcome } from '../pages/Home/Transaction/helpers/useTransactionOutcome';
