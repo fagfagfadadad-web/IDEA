e46b2edbc85ae023a9db158cf0e035b31a2cf187
fagfagfadadad-web <fagfagfadadad@gmail.com>
2025-08-05 15:26:23 +0200

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgrPlugin from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
   server: {
