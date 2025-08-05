0b64209f86fff044dae50e4b5204b798cd1d5549
fagfagfadadad-web <fagfagfadadad@gmail.com>
2025-08-05 15:25:08 +0200

    Remove Node.js polyfills to fix runtime error

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import svgrPlugin from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   server: {
-    port: Number(process.env.PORT) || 3000,
-    strictPort: true,
-    host: true,
-    https: false, // Vypni HTTPS
-    watch: {
-      usePolling: false,
-      useFsEvents: false,
-    },
-    hmr: {
-      overlay: false,
-    },
-  },
-  build: {
-    outDir: 'build',
-    sourcemap: false,
     rollupOptions: {
       output: {
         sourcemap: false,
