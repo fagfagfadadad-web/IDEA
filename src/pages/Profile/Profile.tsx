6f87d86a190d83a61b7233e505aed7933f98f6b4
fagfagfadadad-web <fagfagfadadad@gmail.com>
2025-08-05 12:21:23 +0200

    Updated Profile.tsx

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -21,7 +21,8 @@ import {
   MessageSquare,
   Plus,
   Trash2,
-  MoreVertical,
+  Eye,
+  TrendingUp
   Coins
 } from 'lucide-react';
 import { Button, Card, EmailNotificationsToggle, ReviewsList } from 'components';
