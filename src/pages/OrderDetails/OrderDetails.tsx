3e6f32b13ece4310e117e3fa6e99a40c75bcedfb
fagfagfadadad-web <fagfagfadadad@gmail.com>
2025-08-05 21:14:34 +0200

    Fix provider address access in order creation

--- src/pages/OrderDetails/OrderDetails.tsx
+++ src/pages/OrderDetails/OrderDetails.tsx
@@ -214,6 +214,7 @@ const OrderDetails = () => {
       }
       
       const walletAddress = data?.users?.[0]?.wallet_address;
+      const walletAddress = data?.users?.wallet_address;
       console.log('Fetched provider data:', { data, walletAddress });
       
       if (!isValidAddress(walletAddress)) {
