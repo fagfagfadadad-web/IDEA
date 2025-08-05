420e640fbf9c0f5301ff40f3705e787c26ead868
fagfagfadadad-web <fagfagfadadad@gmail.com>
2025-08-05 23:32:11 +0200

    Added useOrders.ts

--- src/pages/OrderDetails/OrderDetails.tsx
+++ src/pages/OrderDetails/OrderDetails.tsx
@@ -213,7 +213,7 @@ const OrderDetails = () => {
         return null;
       }
       
-      const walletAddress = data?.provider?.wallet_address;
+      const walletAddress = data?.provider?.[0]?.wallet_address;
         
       console.log('Fetched provider data:', { data, walletAddress });
       
