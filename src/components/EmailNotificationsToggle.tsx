dfeb094a75a1ab84e8ad6f12fdbb182abcc3804d
fagfagfadadad-web <fagfagfadadad@gmail.com>
2025-08-07 06:38:09 +0200

    Fix circular dependency in EmailNotificationsToggle

--- src/components/EmailNotificationsToggle.tsx
+++ src/components/EmailNotificationsToggle.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Check } from 'lucide-react';
-import { Button } from 'components';
+import { Button } from './Button';
 
 interface EmailNotificationsToggleProps {
   enabled: boolean;
