720ef199fd45d87f4a3e561c7b29751feb3d9f98
fagfagfadadad-web <fagfagfadadad@gmail.com>
2025-08-08 13:24:56 +0200

    Fix Object.defineProperty error

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// Fix for Object.defineProperty called on non-object error
+// This must be executed BEFORE any other imports that might use global
+if (typeof window !== 'undefined' && typeof (window as any).global === 'undefined') {
+  (window as any).global = window;
+}
+
 import { initApp } from 'lib';
 import { App } from './App';
 import { config } from './initConfig';
