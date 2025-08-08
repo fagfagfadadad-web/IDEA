eba777967bdc9f456d3a584b8f9e29c503c671e1
fagfagfadadad-web <fagfagfadadad@gmail.com>
2025-08-08 10:27:05 +0200

    Updated Header.tsx

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -33,6 +33,14 @@ export const Header = () => {
             <p className='text-gray-600'>{environment}</p>
           </div>
 
+          {/* Token Sale Link */}
+          <a
+            href='https://ideagigs.store/token-sale'
+            className='inline-block rounded-lg px-3 py-2 text-center hover:no-underline my-0 text-gray-600 hover:bg-slate-100 mx-0'
+          >
+            Token Sale
+          </a>
+
           {isLoggedIn && (
             <>
               <NotificationsButton />
