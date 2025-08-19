8e43ac7e2b464d79f2ef7273b98f83c4674fdf89
fagfagfadadad-web <fagfagfadadad@gmail.com>
2025-08-19 11:17:40 +0200

    Updated Builder.tsx

--- src/pages/Builder/Builder.tsx
+++ src/pages/Builder/Builder.tsx
@@ -1,27 +1,25 @@
 .webcontainer@runtime.96435430.js:26 [vite] Internal Server Error
-/home/project/src/pages/Builder/Builder.tsx: Identifier 'Settings' has already been declared. (27:2)
+/home/project/src/pages/Builder/Builder.tsx: Unexpected token (1:0)
 
-  25 |   Trash2,
-  26 |   Type as TypeIcon,
-> 27 |   Settings,
-     |   ^
-  28 |   Zap,
-  29 |   Copy,
-  30 |   Github
+> 1 | .webcontainer@runtime.96435430.js:26 [vite] Internal Server Error
+    | ^
+  2 | /home/project/src/pages/Builder/Builder.tsx: Identifier 'Settings' has already been declared. (27:2)
+  3 |
+  4 |   25 |   Trash2,
     at constructor (/home/project/node_modules/@babel/parser/lib/index.js#cjs:367:19)
     at TypeScriptParserMixin.raise (/home/project/node_modules/@babel/parser/lib/index.js#cjs:6627:19)
-    at TypeScriptScopeHandler.declareName (/home/project/node_modules/@babel/parser/lib/index.js#cjs:4896:21)
-    at TypeScriptParserMixin.declareNameFromIdentifier (/home/project/node_modules/@babel/parser/lib/index.js#cjs:7591:16)
-    at TypeScriptParserMixin.checkIdentifier (/home/project/node_modules/@babel/parser/lib/index.js#cjs:7587:12)
-    at TypeScriptParserMixin.checkLVal (/home/project/node_modules/@babel/parser/lib/index.js#cjs:7526:12)
-    at TypeScriptParserMixin.finishImportSpecifier (/home/project/node_modules/@babel/parser/lib/index.js#cjs:14266:10)
-    at TypeScriptParserMixin.parseImportSpecifier (/home/project/node_modules/@babel/parser/lib/index.js#cjs:14423:17)
-    at TypeScriptParserMixin.parseImportSpecifier (/home/project/node_modules/@babel/parser/lib/index.js#cjs:10166:18)
-    at TypeScriptParserMixin.parseNamedImportSpecifiers (/home/project/node_modules/@babel/parser/lib/index.js#cjs:14402:36)
+    at TypeScriptParserMixin.unexpected (/home/project/node_modules/@babel/parser/lib/index.js#cjs:6647:16)
+    at TypeScriptParserMixin.parseExprAtom (/home/project/node_modules/@babel/parser/lib/index.js#cjs:11442:16)
+    at TypeScriptParserMixin.parseExprAtom (/home/project/node_modules/@babel/parser/lib/index.js#cjs:4794:20)
+    at TypeScriptParserMixin.parseExprSubscripts (/home/project/node_modules/@babel/parser/lib/index.js#cjs:11076:23)
+    at TypeScriptParserMixin.parseUpdate (/home/project/node_modules/@babel/parser/lib/index.js#cjs:11061:21)
+    at TypeScriptParserMixin.parseMaybeUnary (/home/project/node_modules/@babel/parser/lib/index.js#cjs:11041:23)
+    at TypeScriptParserMixin.parseMaybeUnary (/home/project/node_modules/@babel/parser/lib/index.js#cjs:9852:18)
+    at TypeScriptParserMixin.parseMaybeUnaryOrPrivate (/home/project/node_modules/@babel/parser/lib/index.js#cjs:10894:61)
 console.<computed> @ .webcontainer@runtime.96435430.js:26
 handleMessage @ client.ts:265
 (anonymous) @ client.ts:91
 dispatchEvent @ .webcontainer@runtime.96435430.js:26
 _handleMessage @ .webcontainer@runtime.96435430.js:26
 _0x4a5968.<computed> @ .webcontainer@runtime.96435430.js:26
-index.ts:2115  GET https://zp1v56uxy8rdx5ypatb0ockcb9tr6a-oci3--3000--96435430.local-credentialless.webcontainer-api.io/src/pages/Builder/Builder.tsx?t=1755594539852 net::ERR_ABORTED 500 (Internal Server Error)
+index.ts:2115  GET https://zp1v56uxy8rdx5ypatb0ockcb9tr6a-oci3--3000--96435430.local-credentialless.webcontainer-api.io/src/pages/Builder/Builder.tsx?t=1755594817245 net::ERR_ABORTED 500 (Internal Server Error)
