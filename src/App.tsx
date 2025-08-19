840b0d9d4f280462b47b250b4a7b3f3290b33c49
fagfagfadadad-web <fagfagfadadad@gmail.com>
2025-08-19 08:45:39 +0200

    Fix MultiversX SDK import errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { DappProvider } from '@multiversx/sdk-dapp/wrappers/DappProvider';
-import { NotificationModal, SignTransactionsModals, TransactionsToastList } from '@multiversx/sdk-dapp/UI';
-import { EnvironmentsEnum } from '@multiversx/sdk-dapp/types';
 import { Route, Routes } from 'react-router-dom';
 import { Suspense } from 'react';
 import { ToastProvider } from './context/ToastContext';
 import { PageNotFound } from 'pages/PageNotFound/PageNotFound';
 import { routes } from 'routes';
 import { Layout } from './components';
-
-const environment = EnvironmentsEnum.devnet;
-const customNetworkConfig = {
-  id: 'devnet',
-  name: 'Devnet',
-  egldLabel: 'xEGLD',
-  walletAddress: 'https://devnet-wallet.multiversx.com',
-  apiAddress: 'https://devnet-api.multiversx.com',
-  gatewayAddress: 'https://devnet-gateway.multiversx.com',
-  explorerAddress: 'https://devnet-explorer.multiversx.com'
-};
 
 const AppContent = () => {
   return (
@@ -50,21 +36,10 @@ const AppContent = () => {
 
 export const App = () => {
   return (
-    <DappProvider
-      environment={environment}
-      customNetworkConfig={customNetworkConfig}
-      dappConfig={{
-        shouldUseWebViewProvider: true,
-      }}
-    >
-      <ToastProvider>
-        <BrowserRouter>
-          <AppContent />
-          <NotificationModal />
-          <SignTransactionsModals />
-          <TransactionsToastList />
-        </BrowserRouter>
-      </ToastProvider>
-    </DappProvider>
+    <ToastProvider>
+      <BrowserRouter>
+        <AppContent />
+      </BrowserRouter>
+    </ToastProvider>
   );
 };
