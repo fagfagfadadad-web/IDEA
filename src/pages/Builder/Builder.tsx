5ca2024ef1dbbcbf179a7dd18db57b23405c859d
fagfagfadadad-web <fagfagfadadad@gmail.com>
2025-08-19 11:21:52 +0200

    Updated Builder.tsx

--- src/pages/Builder/Builder.tsx
+++ src/pages/Builder/Builder.tsx
@@ -3,9 +3,9 @@
 
 > 1 | .webcontainer@runtime.96435430.js:26 [vite] Internal Server Error
     | ^
-  2 | /home/project/src/pages/Builder/Builder.tsx: Identifier 'Settings' has already been declared. (27:2)
+  2 | /home/project/src/pages/Builder/Builder.tsx: Unexpected token (1:0)
   3 |
-  4 |   25 |   Trash2,
+  4 | > 1 | .webcontainer@runtime.96435430.js:26 [vite] Internal Server Error
     at constructor (/home/project/node_modules/@babel/parser/lib/index.js#cjs:367:19)
     at TypeScriptParserMixin.raise (/home/project/node_modules/@babel/parser/lib/index.js#cjs:6627:19)
     at TypeScriptParserMixin.unexpected (/home/project/node_modules/@babel/parser/lib/index.js#cjs:6647:16)
@@ -22,4 +22,4 @@ handleMessage @ client.ts:265
 dispatchEvent @ .webcontainer@runtime.96435430.js:26
 _handleMessage @ .webcontainer@runtime.96435430.js:26
 _0x4a5968.<computed> @ .webcontainer@runtime.96435430.js:26
-index.ts:2115  GET https://zp1v56uxy8rdx5ypatb0ockcb9tr6a-oci3--3000--96435430.local-credentialless.webcontainer-api.io/src/pages/Builder/Builder.tsx?t=1755594817245 net::ERR_ABORTED 500 (Internal Server Error)
+index.ts:2115  GET https://zp1v56uxy8rdx5ypatb0ockcb9tr6a-oci3--3000--96435430.local-credentialless.webcontainer-api.io/src/pages/Builder/Builder.tsx?t=1755595059092 net::ERR_ABORTED 500 (Internal Server Error)
