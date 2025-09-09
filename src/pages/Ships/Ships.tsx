49d41f0245e0a54b0e724446d76a7544afe587ff
fagfagfadadad-web <fagfagfadadad@gmail.com>
2025-09-09 19:38:20 +0200

    Updated Ships.tsx

--- src/pages/Ships/Ships.tsx
+++ src/pages/Ships/Ships.tsx
@@ -317,9 +317,7 @@ export const Ships = () => {
                                 </div>
                                ) : `Need ${shipType.cost.toLocaleString()} ZEN`}
                             </Button>
-                          <span className="text-green-400 font-bold">
-                            {ship.current_energy || ship.currentEnergy || 0}/{ship.energy_capacity || ship.energyCapacity || 100}
-                          </span>
+                          </div>
                         </div>
                       );
                     })}
