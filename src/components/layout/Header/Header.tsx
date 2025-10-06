45a675edc8dcff8682a5fc8d4511e12ff83bfe6c
fagfagfadadad-web <fagfagfadadad@gmail.com>
2025-10-06 15:37:49 +0200

    Updated Header.tsx

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -29,16 +29,16 @@ export const Header = () => {
 
       <nav className='h-full w-full text-sm sm:relative sm:left-auto sm:top-auto sm:flex sm:w-auto sm:flex-row sm:justify-end sm:bg-transparent'>
         <div className='flex justify-end container mx-auto items-center gap-2'>
-          {isLoggedIn && (
-            <div className='md:hidden'>
-              <Chat inHeader={true} />
-            </div>
-          )}
-
           <div className='flex gap-1 items-center'>
             <div className='w-2 h-2 rounded-full bg-green-500' />
             <p className='text-gray-600'>{environment}</p>
           </div>
+
+          {isLoggedIn && (
+            <div className='block md:hidden'>
+              <Chat inHeader={true} />
+            </div>
+          )}
 
           {isLoggedIn && (
             <Button
