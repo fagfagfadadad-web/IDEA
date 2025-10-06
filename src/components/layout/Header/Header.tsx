02a829ba693c094b761fe7b4f47d1ba00591eba8
fagfagfadadad-web <fagfagfadadad@gmail.com>
2025-10-06 15:47:18 +0200

    Updated Header.tsx

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -17,7 +17,7 @@ export const Header = () => {
   };
 
   return (
-    <header className='flex flex-row align-center justify-between pl-6 pr-6 pt-6'>
+    <header className='flex flex-row items-center justify-between pl-6 pr-6 pt-6'>
       <MxLink
         className='flex items-center justify-between'
         to={isLoggedIn ? RouteNamesEnum.home : RouteNamesEnum.home}
@@ -27,39 +27,37 @@ export const Header = () => {
         </div>
       </MxLink>
 
-      <nav className='h-full w-full text-sm sm:relative sm:left-auto sm:top-auto sm:flex sm:w-auto sm:flex-row sm:justify-end sm:bg-transparent'>
-        <div className='flex justify-end container mx-auto items-center gap-2'>
-          <div className='flex gap-1 items-center'>
-            <div className='w-2 h-2 rounded-full bg-green-500' />
-            <p className='text-gray-600'>{environment}</p>
-          </div>
-
-          {isLoggedIn && (
-            <div className='block md:hidden'>
-              <Chat inHeader={true} />
-            </div>
-          )}
-
-          {isLoggedIn && (
-            <Button
-              onClick={handleLogout}
-              className='inline-block rounded-lg px-3 py-2 text-center hover:no-underline my-0 text-gray-600 hover:bg-slate-100 mx-0'
-            >
-              Close
-            </Button>
-          )}
-
-          {!isLoggedIn && (
-            <Button
-              onClick={() => {
-                navigate(RouteNamesEnum.unlock);
-              }}
-            >
-              Connect
-            </Button>
-          )}
+      <div className='flex items-center gap-2'>
+        <div className='flex gap-1 items-center'>
+          <div className='w-2 h-2 rounded-full bg-green-500' />
+          <p className='text-gray-600 text-sm'>{environment}</p>
         </div>
-      </nav>
+
+        {isLoggedIn && (
+          <div className='block md:hidden'>
+            <Chat inHeader={true} />
+          </div>
+        )}
+
+        {isLoggedIn && (
+          <Button
+            onClick={handleLogout}
+            className='inline-block rounded-lg px-3 py-2 text-center hover:no-underline my-0 text-gray-600 hover:bg-slate-100 mx-0 text-sm'
+          >
+            Close
+          </Button>
+        )}
+
+        {!isLoggedIn && (
+          <Button
+            onClick={() => {
+              navigate(RouteNamesEnum.unlock);
+            }}
+          >
+            Connect
+          </Button>
+        )}
+      </div>
     </header>
   );
 };
