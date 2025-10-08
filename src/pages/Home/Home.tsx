ed5634f6d93d1e4ca4083fdbce534ce112b63cbb
fagfagfadadad-web <fagfagfadadad@gmail.com>
2025-10-08 14:20:06 +0200

    Updated globals.css

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -98,7 +98,7 @@ export const Home = () => {
             {/* Logo and Title */}
             <div className="flex flex-col items-center space-y-2">
               <div className="w-full flex justify-center">
-                <div className="w-48 h-48 md:w-64 md:h-64 flex items-center justify-center">
+                <div className="w-48 h-48 md:w-64 md:h-64 flex items-center justify-center animate-float">
                   <img src='/pupfi new (1) (1).png' alt='PupFi' className='w-full h-auto object-contain' />
                 </div>
               </div>
